450435f8daffd1bfa0198017ebef9c738565ea39
Alice Chen <alice@perka.com>
2021-06-25 15:26:15 +0800

    Missing quotes

--- app.py
+++ app.py
@@ -50,20 +50,20 @@ def handle_message(event):
     restaurant, date = tokens[1:]
     user_line_id = event.source.user_id
     print(user_line_id)
-    print(new_user(user_line_id))
-    print(new_bento(find_user(user_line_id), find_restaurant(name), date))
+    new_user(user_line_id)
+    new_bento(find_user(user_line_id), find_restaurant(name), date)
   line_bot_api.reply_message(event.reply_token, TextSendMessage(text=response))
 
 def find_restaurant(name):
   cur = conn.cursor()
-  cur.execute("SELECT id FROM restauratns WHERE name = %s;", (name))
+  cur.execute("SELECT id FROM restauratns WHERE name = '%s';", (name))
   id = cur.fetchone()[0]
   cur.close()
   return id
 
 def find_user(line_id):
   cur = conn.cursor()
-  cur.execute("SELECT id FROM users WHERE line_id = %s;", (line_id))
+  cur.execute("SELECT id FROM users WHERE line_id = '%s';", (line_id))
   id = cur.fetchone()[0]
   cur.close()
   return id
@@ -71,16 +71,14 @@ def find_user(line_id):
 def new_user(line_id, name='Alice Chen'):
   cur = conn.cursor()
   # new user entry
-  val = cur.execute("INSERT INTO users (line_id, name, created_at) VALUES (%s, %s, %s);", (line_id, name, datetime.now()))
+  cur.execute("INSERT INTO users (line_id, name, created_at) VALUES (%s, %s, %s);", (line_id, name, datetime.now()))
   cur.close()
-  return val
 
 def new_bento(user_id, restaurant_id, order_date):
   cur = conn.cursor()
   # new bento entry
-  val = cur.execute("INSERT INTO bentos (user_id, restaurant_id, order_date) VALUES (%s, %s, %s);", (user_id, restaurant_id, order_date))
+  cur.execute("INSERT INTO bentos (user_id, restaurant_id, order_date) VALUES (%s, %s, %s);", (user_id, restaurant_id, order_date))
   cur.close()
-  return val
 
 if __name__ == '__main__':
   app.run()
