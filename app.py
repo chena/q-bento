686398348d7fe60700f0e637b3230cdd0ad7baa4
Alice Chen <alice@perka.com>
2021-06-26 11:02:37 +0800

    Check get or create user/restaurant

--- app.py
+++ app.py
@@ -48,25 +48,40 @@ def handle_message(event):
 
   if (tokens[0].startswith('bento') and len(tokens) > 2):
     restaurant, date = tokens[1:]
-    user_line_id = event.source.user_id
-    print(user_line_id)
-    # new_user(user_line_id)
-    new_bento(find_user(user_line_id), find_restaurant(restaurant), date)
+    user_id = get_or_create_user(event.source.user_id)
+    restaurant_id = get_or_create_restaurant(restaurant)
+    new_bento(user_id, restaurant_id, date)
   line_bot_api.reply_message(event.reply_token, TextSendMessage(text=response))
+
+def get_or_create_restaurant(name):
+  found_rest = find_restaurant(name)
+  if found_rest:
+    return found_rest
+  else:
+    return new_restaurant(name)
+
+def get_or_create_user(line_id):
+  found_user = find_user(line_id)
+  if found_user:
+    return found_user
+  else:
+    return new_user(line_id)
 
 def find_restaurant(name):
   cur = conn.cursor()
   cur.execute("SELECT id FROM restaurants WHERE name = %s;", (name,))
-  id = cur.fetchone()[0]
+  res = cur.fetchone()
   cur.close()
-  return id
+  if res:
+    return res[0]
 
 def find_user(line_id):
   cur = conn.cursor()
   cur.execute("SELECT id FROM users WHERE line_id = %s;", (line_id,))
-  id = cur.fetchone()[0]
+  res = cur.fetchone()
   cur.close()
-  return id
+  if res:
+    return res[0]
 
 def new_user(line_id, name='Alice Chen'):
   cur = conn.cursor()
@@ -79,6 +94,13 @@ def new_bento(user_id, restaurant_id, order_date):
   cur = conn.cursor()
   # new bento entry
   cur.execute("INSERT INTO bentos (user_id, restaurant_id, order_date, created_at) VALUES (%s, %s, %s, %s);", (user_id, restaurant_id, order_date, datetime.now()))
+  conn.commit()
+  cur.close()
+
+def new_restaurant(name):
+  cur = conn.cursor()
+  # new bento entry
+  cur.execute("INSERT INTO restaurant (name, created_at) VALUES (%s, %s);", (name, datetime.now()))
   conn.commit()
   cur.close()
 
