2327709670a5e41c8dd83116a34a6dca7f24674c
Alice Chen <alice@perka.com>
2021-06-26 13:17:19 +0800

    Fix get or create

--- app.py
+++ app.py
@@ -80,14 +80,16 @@ def get_or_create_restaurant(name):
   if found_rest:
     return found_rest
   else:
-    return new_restaurant(name)
+    new_restaurant(name)
+    return find_restaurant(name)
 
 def get_or_create_user(line_id):
   found_user = find_user(line_id)
   if found_user:
     return found_user
   else:
-    return new_user(line_id)
+    new_user(line_id)
+    return find_user(line_id)
 
 def find_restaurant(name):
   return __get_first_row("SELECT id FROM restaurants WHERE name = %s;", (name,))
