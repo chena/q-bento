dbc3a0824316aec1ec1c3106def9ad53e9d004d1
Alice Chen <alice@perka.com>
2021-07-05 13:59:53 +0800

    Missing ImageMessage import

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from linebot.exceptions import (
 )
 
 from linebot.models import (
-  MessageEvent, TextMessage, TextSendMessage
+  MessageEvent, TextMessage, TextSendMessage, ImageMessage
 )
 
 DATABASE_URL = os.environ['DATABASE_URL']
