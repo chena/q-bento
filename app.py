e80522f1bc03150da17ea50f3467ec79f4020792
Alice Chen <alice@perka.com>
2021-07-16 22:01:14 +0800

    Try again

--- app.py
+++ app.py
@@ -37,6 +37,10 @@ headers = {
   "Authorization": "Bearer " + TOKEN
 }
 
+line_bot_api.push_message(os.environ['LINE_USER_ID'], TextSendMessage(text='PUSH!!'))
+schedule.every().day.at('22:02').do(line_bot_api.push_message(os.environ['LINE_USER_ID'], TextSendMessage(text='今天吃什麼呢？')))
+schedule.every(1).minutes.do(line_bot_api.push_message(os.environ['LINE_USER_ID'], TextSendMessage(text='Hello!!')))
+
 def daily_push():
   print('PUSH')
   line_bot_api.push_message(os.environ['LINE_USER_ID'], TextSendMessage(text='今天吃什麼呢？'))
@@ -386,6 +390,3 @@ if __name__ == '__main__':
   # while True:
   #   schedule.run_pending()
   #   time.sleep(1)
-  line_bot_api.push_message(os.environ['LINE_USER_ID'], TextSendMessage(text='PUSH!!'))
-  schedule.every().day.at('22:00').do(daily_push)
-  schedule.every(1).minutes.do(line_bot_api.push_message(os.environ['LINE_USER_ID'], TextSendMessage(text='Hello!!')))
