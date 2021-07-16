1992745c36196f27137b5cc9df68cb63772b3e22
Alice Chen <alice@perka.com>
2021-07-16 21:42:29 +0800

    Missing requirement

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ while True:
   schedule.run_pending()
   time.sleep(1)
 line_bot_api.push_message(os.environ['LINE_USER_ID'], TextSendMessage(text='PUSH!!'))
-schedule.every().day.at('21:42').do(daily_push)
+schedule.every().day.at('21:44').do(daily_push)
 schedule.every(1).minutes.do(line_bot_api.push_message(os.environ['LINE_USER_ID'], TextSendMessage(text='Hello!!')))
 
 def daily_push():
