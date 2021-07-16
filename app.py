5861507dc8a3ab3895a8422fe64a2d5fe08322ee
Alice Chen <alice@perka.com>
2021-07-17 00:29:00 +0800

    Edit labels

--- app.py
+++ app.py
@@ -47,16 +47,21 @@ scheduler.start()
 
 @scheduler.task('cron', id='daily_push', hour='3')
 def daily_push():
-  line_bot_api.push_message(LINE_GROUP_ID, TextSendMessage(text='今天吃什麼呢？'))
-
-@scheduler.task('cron', id='test_push', hour='16', minute='25')
-def daily_push():
   line_bot_api.push_message(os.environ['LINE_USER_ID'], TextSendMessage(
-    text='今天吃什麼呢？', quick_reply=QuickReply(items=[
-      QuickReplyButton(action=MessageAction(label="Q便當隨機選🤖", text="bento pick")),
-      QuickReplyButton(action=MessageAction(label="看看想吃清單🍱", text="bento what"))
+    text='早安☀️今天吃什麼呢？', quick_reply=QuickReply(items=[
+      QuickReplyButton(action=MessageAction(label="🤖 Q便當隨機選", text="bento pick")),
+      QuickReplyButton(action=MessageAction(label="🍱 看看想吃清單", text="bento what"))
     ])
   ))
+
+# @scheduler.task('cron', id='test_push', hour='16', minute='25')
+# def daily_push():
+#   line_bot_api.push_message(os.environ['LINE_USER_ID'], TextSendMessage(
+#     text='早安☀️今天吃什麼呢？', quick_reply=QuickReply(items=[
+#       QuickReplyButton(action=MessageAction(label="Q便當隨機選🤖", text="bento pick")),
+#       QuickReplyButton(action=MessageAction(label="看看想吃清單🍱", text="bento what"))
+#     ])
+#   ))
 
 @app.route('/callback', methods=['POST'])
 def callback():
